e9c9a807864bb0bd9c1b75969421c7f06bfba445
victoralcan <victoralcan@gmail.com>
2016-09-05 14:17:00 -0300

    Update elevador.h

--- elevador.h
+++ elevador.h
@@ -2,3 +2,20 @@ typedef int bool;
 #define true 1
 #define false 0
 
+typedef struct {
+	int andar_entrada;
+	int andar_destino;
+	int numero; 
+	int tempo_entrada; 
+	int tempo_saida;
+	int tempo_chamada;
+	bool dentro;
+	/*Talvez outra booleana para identificar se está subindo ou descendo*/
+} Passageiro;
+
+typedef struct{
+	int andar_atual;
+	int passageiros;
+	bool subindo;
+}elevador;
+	
