03893a4bfe646a01fef6e2c5fb94476b9d6cf972
victoralcan <victoralcan@gmail.com>
2016-09-05 15:04:06 -0300

    Update elevador.h

--- elevador.h
+++ elevador.h
@@ -16,6 +16,14 @@ typedef struct {
 typedef struct{
 	int andar_atual;
 	int passageiros;
+	int ocupantes;
 	bool subindo;
 }elevador;
-	
+
+void elevadorDescendo(); /*Tratamento do elevador descendo*/
+void elevadorSubindo(); /*Tratamento do elevador subindo*/
+void elevadorParado(); /*Não tem nenhuma chamada pendente, mas continua incrementando o tempo*/
+void aoEntrar(); /*O que fazer ao entrar (ordenar vetor de prioridades e passageiros)*/
+void trataTempo(); /*Calcula os tempos de viagem e etc para imprimir*/
+void escreveLog();/*Escreve no arquivo texto de saida dos eventos*/
+
