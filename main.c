2e23f52cf87ae4543829a9b583a41b9cc2701427
victoralcan <victoralcan@gmail.com>
2016-09-08 14:25:51 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include "elevador.h"
 
+/*Argv[1] é elevador*/
+/*Argv[2] é eventos*/
+
 int main(int agrc, char *argv[])
 {
+	int capacidade;
 	Elevador elevador;
+	Passageiro *embarcados;
+	inicializaElevador(elevador, argv[1]);
+	embarcados = (Passageiro*) malloc(sizeof(Passageiro) * elevador.capacidade); 
+	embarcadosVazio(embarcados,argv[1]); /*Preenche o vetor*/
+	
+
+
+
+
+
+
+
+
+
+
+
+
+	free(embarcados);
 	return 0;
 }
