edb66aac9cfd053fd3b813d601b88883fcc27247
victoralcan <victoralcan@gmail.com>
2016-09-08 13:00:32 -0300

    Update elevador.h

--- elevador.h
+++ elevador.h
@@ -11,20 +11,34 @@ typedef struct {
 	int tempo_chamada;
 	bool dentro;
 	/*Talvez outra booleana para identificar se está subindo ou descendo*/
-} Passageiro;
+}Passageiro;
 
 typedef struct{
 	int andar_atual;
-	int passageiros;
+	int total_andares;
 	int ocupantes;
+	int capacidade;
 	bool subindo;
-}elevador;
+}Elevador;
+
+typedef struct celula {
+  int item;
+  struct celula *proximo;
+  struct celula *ant;
+} celula;
+
+typedef struct tlista{
+  struct celula *primeiro;
+  struct celula *ultimo;
+}tlista;
 
 void elevadorDescendo(); /*Tratamento do elevador descendo*/
 void elevadorSubindo(); /*Tratamento do elevador subindo*/
 void elevadorParado(); /*Não tem nenhuma chamada pendente, mas continua incrementando o tempo*/
 void aoEntrar(); /*O que fazer ao entrar (ordenar vetor de prioridades e passageiros)*/
+void aoSair(); /*Executas certas acoes ao deixar o passageiro no destino*/
 void trataTempo(); /*Calcula os tempos de viagem e etc para imprimir*/
 void escreveLog();/*Escreve no arquivo texto de saida dos eventos*/
-void IniciaElevador(); /*Inicializa o elevador*/
+void iniciaElevador(); /*Inicializa o elevador*/
 void leArquivos(); /*Lê os arquivos de ambiente e eventos*/
+void embarcadosVazio(); /*Preenche o vetor de embarcados*/
