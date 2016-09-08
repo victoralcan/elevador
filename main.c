1d82467d5fb52f777397d72ffa77b12a47ed4910
victoralcan <victoralcan@gmail.com>
2016-09-08 15:32:27 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "elevador.h"
 
 /*Argv[1] é elevador*/
 /*Argv[2] é eventos*/
 
-int main(int agrc, char *argv[])
+int main()
 {
-	int capacidade;
-	Elevador elevador;
+	int capacidade,andares;
+	Elevador *elevador;
 	Passageiro *embarcados;
-	inicializaElevador(elevador, argv[1]);
-	embarcados = (Passageiro*) malloc(sizeof(Passageiro) * elevador.capacidade); 
-	embarcadosVazio(embarcados,argv[1]); /*Preenche o vetor*/
-	
+	leArquivos(&capacidade,&andares);
+	printf("%d %d\n",andares, capacidade);
+	iniciaElevador(elevador, capacidade, andares);
+	embarcados = (Passageiro*) malloc(sizeof(Passageiro) * elevador->capacidade); 
+	embarcadosVazio(embarcados,capacidade); 
+	printf("andar atual%d\n", elevador->andar_atual);
+	printf("ocupantes%d\n", elevador->ocupantes);
+	printf("subindo?%d\n", elevador->subindo);
+	printf("andares%d\n", elevador->total_andares);
+	printf("capacidade%d\n", elevador->capacidade);
+	for(int i=0;i<capacidade;i++)
+	{
+		printf("andar entrada pass %d %d\n",i+1, embarcados[i].andar_entrada);
+		printf("andar destino pass %d %d\n",i+1, embarcados[i].andar_destino);
+		printf("numero pass %d %d\n",i+1, embarcados[i].numero);
+		printf("tempo entrada pass %d %d\n",i+1, embarcados[i].tempo_entrada);
+		printf("tempo saida pass %d %d\n",i+1, embarcados[i].tempo_saida);
+		printf("dentro pass %d? %d\n",i+1, embarcados[i].dentro);
+	}
+
+
 
 
 
