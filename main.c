ce4cd61b1196e8536733e4aec7625b4fa8988c5c
victoralcan <victoralcan@gmail.com>
2016-09-09 18:21:06 -0300

    Update main.c

--- main.c
+++ main.c
@@ -7,20 +7,19 @@
 
 int main()
 {
-	int capacidade,andares;
 	Elevador *elevador;
 	Passageiro *embarcados;
-	leArquivos(&capacidade,&andares);
-	printf("%d %d\n",andares, capacidade);
-	iniciaElevador(elevador, capacidade, andares);
+	leArquivos(elevador);
+	iniciaElevador(elevador);
+
 	embarcados = (Passageiro*) malloc(sizeof(Passageiro) * elevador->capacidade); 
-	embarcadosVazio(embarcados,capacidade); 
-	printf("andar atual%d\n", elevador->andar_atual);
-	printf("ocupantes%d\n", elevador->ocupantes);
-	printf("subindo?%d\n", elevador->subindo);
-	printf("andares%d\n", elevador->total_andares);
-	printf("capacidade%d\n", elevador->capacidade);
-	for(int i=0;i<capacidade;i++)
+	/*embarcadosVazio(embarcados,capacidade); *//*seg*/
+	printf("andar atual %d\n", elevador->andar_atual);
+	printf("ocupantes %d\n", elevador->ocupantes);
+	printf("subindo? %d\n", elevador->subindo);
+	printf("andares %d\n", elevador->andares);
+	printf("capacidade %d\n", elevador->capacidade);
+	/*for(int i=0;i<capacidade;i++)
 	{
 		printf("andar entrada pass %d %d\n",i+1, embarcados[i].andar_entrada);
 		printf("andar destino pass %d %d\n",i+1, embarcados[i].andar_destino);
@@ -29,20 +28,7 @@ int main()
 		printf("tempo saida pass %d %d\n",i+1, embarcados[i].tempo_saida);
 		printf("dentro pass %d? %d\n",i+1, embarcados[i].dentro);
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	*/
 	free(embarcados);
 	return 0;
 }
