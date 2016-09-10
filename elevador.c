91cf3ee8f7f6f1c3c40932824681a02dfb6f709e
victoralcan <victoralcan@gmail.com>
2016-09-10 13:55:35 -0300

    Update elevador.c

--- elevador.c
+++ elevador.c
@@ -50,19 +50,19 @@ void leArquivos(Elevador *elevador) /*Lê os arquivos do elevador e eventos*/
 	}
 }
 
-void embarcadosVazio(Passageiro embarcados[],Elevador *elevador)
+void embarcadosVazio(Passageiro embarcados[], int capacidade)
 {
-	Passageiro *vazio;
-	vazio->andar_entrada = -1;
-	vazio->andar_destino = -1;
-	vazio->numero = -1;
-	vazio->tempo_entrada = -1;
-	vazio->tempo_saida = -1;
-	vazio->tempo_chamada = -1;
-	vazio->dentro = false;
-	for(int a=0;a<elevador->capacidade;a++)
+	Passageiro vazio;
+	vazio.andar_entrada = -1;
+	vazio.andar_destino = -1;
+	vazio.numero = 0;	
+	vazio.tempo_entrada = -1;
+	vazio.tempo_saida = -1;
+	vazio.tempo_chamada = -1;
+	vazio.dentro = false;
+	for(int a=0;a<capacidade;a++)
 	{
-		embarcados[a] = *vazio;
+		embarcados[a] = vazio;
 	}
 }
 
@@ -71,4 +71,76 @@ void iniciaElevador(Elevador *elevador) /*Inicializa o elevador*/
   elevador->andar_atual = -1;
   elevador->ocupantes = 0;
   elevador->subindo = false;
+}
+tlista* criaLista()
+{
+	tlista *nova = (tlista*) malloc(sizeof(tlista));
+	nova->ultimo = NULL;
+	nova->primeiro = NULL;
+	return nova;
+}
+void preencheEventos(tlista *lista_eventos, int *num_eventos) /*Lê o arquivo de eventos, preenche vetor, ordena e preenche lista*/
+{
+	FILE *fp;
+	Passageiro eventos;
+	Item item;
+	int aux,a1,a2,a3,num=1;;
+	fp = fopen("eventos.txt","r");
+	if(fp == NULL)/*COLOQUEI OS DOIS IGUAIS CERTOS AGR SEU FDP*/
+	{
+		printf("Não foi possivel abrir o arquivo de eventos\n");
+	}
+	else
+	{
+		while(!feof(fp)) /*Enquanto nao chegar no fim do arquivo*/
+		{
+			fscanf(fp,"%d %d %d\n",&aux,&a1,&a2);
+			*num_eventos++;
+		}
+
+		eventos = (Passageiro*)malloc(sizeof(Passageiro) * (*num_eventos));
+		rewind(fp);
+
+		for(aux = 0;aux < *num_eventos;aux++)   /*Preencher o vetor com os dados do arquivo*/
+		{
+			fscanf(fp,"%d %d %d\n",&eventos[aux].andar_entrada,&eventos[aux].andar_destino,&eventos[aux].tempo_chamada);
+		}
+
+		fclose(fp);
+		qsort();/*Ordenar o vetor de acordo com o tempo*/
+
+		for(aux=0;aux<*num_eventos;aux++) /*Preencher a lista*/
+		{
+			item.passageiro = eventos[aux];
+			item.passageiro.numero = num;
+			if(item.passageiro.andar_entrada<item.passageiro.andar_destino)
+			{
+				item.passageiro.subindo = true;
+			}
+			else
+			{
+				item.passageiro.subindo=false;
+			}
+			num++;
+			/*Função de inserir o item na lista*/
+
+		}
+		free(eventos);	
+	}
+}
+int compara(const void* x,const void* y) /*Funcao parametro para a qsort*/
+{
+	const Passageiro *x1 = (const Passageiro*) x;
+	const Passageiro *y1 = (const Passageiro*) y;
+
+	if (x1->tempo_chamada < y1->tempo_chamada)
+	{ 
+		return -1;
+	}
+	if (x1->tempo_chamada > y1->tempo_chamada)
+	{
+	 	return  1;
+	}
+	
+	return 0;
 }
