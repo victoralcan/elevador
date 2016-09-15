c7d18f5affeb40fea65012c31ca227b983db9dfc
victoralcan <victoralcan@gmail.com>
2016-09-15 15:12:40 -0300

    Update main.c

--- main.c
+++ main.c
@@ -7,15 +7,26 @@ int main()
 {
 	Elevador elevador;
 	Passageiro *embarcados;
+	tlista *subindo;
+	tlista *prox_subindo;
+	tlista *descendo;
+	tlista *prox_descendo;
 	tlista *lista_eventos;
 	int num_eventos = 0;
+
+
 	leArquivos(&elevador);
 	iniciaElevador(&elevador);
 	embarcados = (Passageiro*) malloc(sizeof(Passageiro) * elevador.capacidade); 
 	embarcadosVazio(embarcados,elevador.capacidade); /*RX SENHOR POR FAVOR*/
 	lista_eventos = criaLista();
-	preencheEventos(lista_eventos,&num_eventos);
+	subindo = criaLista();
+	prox_subindo = criaLista();
+	descendo = criaLista();
+	prox_descendo = criaLista();
+	preencheEventos(lista_eventos,subindo,descendo,&num_eventos);
 	printf("%d \n", lista_eventos->primeiro->item.passageiro.tempo_chamada);
+	printf("%d \n", lista_eventos->ultimo->item.passageiro.tempo_chamada);
 	/*while(num_eventos > 0)
 	{
 		elevadorSubindo(&elevador,lista_eventos,embarcados,&num_eventos);         /*Primeiro subindo porque ele começa no andar 0*/
