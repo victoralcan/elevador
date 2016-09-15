e1e76df28cb66f0d0ffc7ba750a0fef3f54301bc
victoralcan <victoralcan@gmail.com>
2016-09-15 12:50:38 -0300

    Update main.c

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int main()
 	embarcadosVazio(embarcados,elevador.capacidade); /*RX SENHOR POR FAVOR*/
 	lista_eventos = criaLista();
 	preencheEventos(lista_eventos,&num_eventos);
-	printf("%d \n", lista_eventos->primeiro->item->passageiro.tempo_chamada);
+	printf("%d \n", lista_eventos->primeiro->item.passageiro.tempo_chamada);
 	/*while(num_eventos > 0)
 	{
 		elevadorSubindo(&elevador,lista_eventos,embarcados,&num_eventos);         /*Primeiro subindo porque ele começa no andar 0*/
