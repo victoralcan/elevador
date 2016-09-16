110394cdacbffbbe94d678f03fc26cc28330bf5c
victoralcan <victoralcan@gmail.com>
2016-09-16 18:21:57 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,43 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "elevador.h"
-
+#include <sys/time.h>
 
 int main()
 {
+	FILE *fp;
 	Elevador elevador;
 	Passageiro *embarcados;
-	tlista *subindo;
-	tlista *prox_subindo;
+	tlista *entradas;
+	tlista *destinos;
 	tlista *descendo;
 	tlista *prox_descendo;
 	tlista *lista_eventos;
 	int num_eventos = 0;
 
+	fp = fopen("log.txt","r");
+	if(fp != NULL)
+	{
+		remove("log.txt");
+	}
+
+	struct timeval inicio, final;
+  	int tmili;
+	gettimeofday(&inicio, NULL);
 
 	leArquivos(&elevador);
 	iniciaElevador(&elevador);
 	embarcados = (Passageiro*) malloc(sizeof(Passageiro) * elevador.capacidade); 
 	embarcadosVazio(embarcados,elevador.capacidade); /*RX SENHOR POR FAVOR*/
 	lista_eventos = criaLista();
-	subindo = criaLista();
-	prox_subindo = criaLista();
-	descendo = criaLista();
-	prox_descendo = criaLista();
-	preencheEventos(lista_eventos,subindo,descendo,&num_eventos);
-	printf("%d \n", lista_eventos->primeiro->item.passageiro.tempo_chamada);
-	printf("%d \n", lista_eventos->ultimo->item.passageiro.tempo_chamada);
-	/*while(num_eventos > 0)
-	{
-		elevadorSubindo(&elevador,lista_eventos,embarcados,&num_eventos);         /*Primeiro subindo porque ele começa no andar 0*/
-		/*elevadorDescendo(&elevador,lista_eventos,embarcados,&num_eventos);
-	}*/
+	entradas = criaLista();
+	destinos = criaLista();
+	/*prox_subindo = criaLista();
+	prox_descendo = criaLista();*/
+	preencheEventos(lista_eventos,entradas,destinos,&num_eventos);
+	fcfs(&elevador,lista_eventos,embarcados,&num_eventos);
 
 
-
-
-
-	
+	free(lista_eventos);
+	free(entradas);
+	free(destinos);
 	free(embarcados);
+
+
+	gettimeofday(&final, NULL);
+    tmili = (int) (1000 * (final.tv_sec - inicio.tv_sec) + (final.tv_usec - inicio.tv_usec) / 1000);
+    printf("Tempo decorrido durante execução: %d milissegundos\n", tmili);
+
+
 	return 0;
 }
